9b15253c3d3816f3839710e6e5dbcb71710b228b
BoikoMykola <ukrelteh@gmail.com>
2021-06-04 13:03:08 +0300

    fixed all comments.

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
-let menuIcon = document.querySelector(".menuIcon");
+const menuIcon = document.querySelector(".menu-icon");
 menuIcon.addEventListener("click", function () {
-    let navigate = document.getElementsByTagName("nav");
+    const navigate = document.getElementsByTagName("nav");
     document.body.classList.toggle("lock")
     navigate[0].classList.toggle("active");
     menuIcon.classList.toggle("active")
 });
 
+
+// MENU HIDDEN
+let previous = 0;
+window.addEventListener("scroll", function () {
+    const menu = document.getElementsByClassName("header-container");
+    const subNav = document.getElementsByClassName("sub-nav-block");
+    if (pageYOffset > 670 && pageYOffset < previous) {
+        document.getElementById("logo").src = "image/SKOUT2.png";
+        menu[0].classList.add("header-container-fixed");
+        subNav[0].classList.add("sub-nav-fixed");
+    } else {
+        document.getElementById("logo").src = "image/SKOUT.png";
+        menu[0].classList.remove("header-container-fixed");
+        subNav[0].classList.remove("sub-nav-fixed");
+    }
+    previous = pageYOffset;
+});
+
+
 // ACCORDION
-
-let accordionHeader = document.getElementsByClassName('accordionHeader'),
-    active = document.getElementsByClassName('activePanel');
-
-Array.from(accordionHeader).forEach(function(item) {
-    item.addEventListener('click', function() {
-        if (active.length > 0 && active[0] !== this.parentNode)
-        {
-            console.log(active[0].classList)
-            active[0].classList.remove('activePanel');
+const accordionHeader = document.getElementsByClassName('accordion-header'),
+    active = document.getElementsByClassName('active-panel');
+Array.from(accordionHeader).forEach(function (item) {
+    item.addEventListener('click', function () {
+        if (active.length > 0 && active[0] !== this.parentNode) {
+            active[0].classList.remove('active-panel');
         }
-        this.parentNode.classList.toggle('activePanel');
+        this.parentNode.classList.toggle('active-panel');
     });
 });
 
 // FAQs
-
-let question = document.getElementsByClassName('question'),
+const question = document.getElementsByClassName('question'),
     blockActive = document.getElementsByClassName('blockActive');
-
-Array.from(question).forEach(function(item) {
-    item.addEventListener('click', function() {
-        if (blockActive.length > 0 && blockActive[0] !== this.parentNode)
-        {
+Array.from(question).forEach(function (item) {
+    item.addEventListener('click', function () {
+        if (blockActive.length > 0 && blockActive[0] !== this.parentNode) {
             blockActive[0].classList.remove('blockActive');
         }
         this.parentNode.classList.toggle('blockActive');
@@ -38,116 +50,118 @@ Array.from(question).forEach(function(item) {
 });
 
 // GLOSSARY
-
-let arr = [
-    {li:"IP Address", text: "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. "},
-    {li: "Malware", text: "Malicious software that blocks access to a system until a ransom is paid."},
-    {li: "Penetration Test", text: "Code that is masked so that a human or computer unintentionally"},
-    {li: "Phishing/ Spearphishi", text: `Lorem ipsum mauris integer in nam gravida adipiscing ultricies, vitae `},
-    {li:"Ransomware", text: "Malicious software that blocks access to a system until a ransom is paid."},
-    {li:"Botnet", text: "Lorem, ipsum tempus rutrum tellus lorem sed nulla gravida bibendum "},
-    {li:"Breach", text: "Lorem ipsum risus non, molestie nec mauris a quisque vulputate. "},
-    {li:"Cloud", text: "Lorem ipsum a enim, elementum leo non: cursus a proin "},
-    {li:"DDoS", text: "Lorem ipsum eu sodales lorem nam sodales adipiscing eros nibh "},
-    {li:"Domain", text: "Malicious software that blocks access to a system until a ransom is paid."},
-    {li:"Endpoint", text: "Lorem ipsum a enim, elementum leo non: cursus a proin "},
-    {li:"Exploit", text: "Lorem ipsum risus non, molestie nec mauris a quisque vulputate, "},
-    {li:"Firewall", text: "Lorem ipsum et eget sagittis nam sem donec porttitor bibendum. "},
-    {li:"Session Hijacking", text: "Malicious software that blocks access to a system until a ransom is paid."},
-    {li:"Social Engineering", text: "Lorem ipsum gravida auctor sit amet commodo mauris sapien justo."},
-    {li:"Software", text: "Lorem ipsum gravida auctor sit amet commodo mauris sapien justo."},
-    {li:"SOC", text: "Malicious software that blocks access to a system until a ransom is paid."},
-    {li:"Trojan Horse", text: "Lorem ipsum et eget sagittis nam sem donec porttitor bibendum."},
-    {li:"VPN", text: "Lorem ipsum gravida auctor sit amet commodo mauris sapien justo"},
-    {li:"Virus", text: "Code that is masked so that a human or computer unintentionally allows it to replicate itself to spread for the purpose of compromising, corrupting, or destroying a system. "},
-    {li:"Worm", text: "Malicious software that blocks access to a system until a ransom is paid."},
-]
+let arr = [];
 let i = 0;
 let listLength = 5;
-if(document.documentElement.clientWidth >= 1440)
-{listLength = 16}
+let change = false;
+if (document.documentElement.clientWidth >= 1440) {
+    listLength = 16;
+    change = true
+}
+
+// Получение информации с JSON - файла
+function sendRequest() {
+    let request;
+    if (window.XMLHttpRequest) {
+        request = new XMLHttpRequest();
+    } else {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open("GET", "arr.json");
+    request.onerror = function () {
+        alert("Ошибка");
+    }
+    request.send();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            arr = JSON.parse(this.response);
+            getList(listLength, arr)
+        }
+    }
+}
+
+// Загрузка нового или предыдущего списка слайдов
+let next = document.getElementsByClassName("next-btn");
+let prev = document.getElementsByClassName("prev-btn");
+
+next[0].addEventListener('click', getNextList);
+prev[0].addEventListener('click', getPrevList);
+
+function getNextList() {
+    let sliderList = document.getElementById("sliderList");
+    let lastElem = sliderList.lastChild.innerHTML
+    let index = arr.findIndex(item => item.name === lastElem);
+    i = index + 1;
+    let listLengthNew = i + listLength;
+    if (listLengthNew < arr.length) {
+        getList(listLengthNew, arr)
+    } else {
+        getList(arr.length, arr)
+    }
+}
+
+function getPrevList() {
+    let sliderList = document.getElementById("sliderList");
+    let firstElem = sliderList.firstChild.innerHTML
+    let index = arr.findIndex(item => item.name === firstElem);
+    i = index - listLength;
+    if (i >= 0) {
+        getList(index, arr)
+    } else {
+        i = 0
+        getList(listLength, arr)
+    }
+}
 
 
-// let rightA = document.getElementsByClassName("arrow1");
-// let leftA = document.getElementsByClassName("arrow2");
-//
-// rightA[0].addEventListener('click', getNewList);
-// leftA[0].addEventListener('click', getOldList);
-//
-// function getNewList() {
-//     i = i + 5;
-//     if(i<arr.length){
-//         getList(listLength, arr);
-//     } else {
-//         i = arr.length;
-//     }
-// }
-//
-// function getOldList() {
-//     i = i - 5;
-//     if(i <= 0){
-//         getList(listLength, arr);
-//     } else {
-//         i = 0;
-//     }
-// }
+// Изменение величины списка слайдов при переходе через breakpoint 1440px.
+window.addEventListener(`resize`, () => {
+    let change2 = document.documentElement.clientWidth >= 1440;
+    if (change !== change2 && document.documentElement.clientWidth >= 1440) {
+        change = change2;
+        listLength = 16;
+        getList(listLength, arr)
+    } else if (change !== change2 && document.documentElement.clientWidth < 1440) {
+        change = change2;
+        listLength = 5;
+        getList(listLength, arr)
+    }
+});
 
-
-// window.addEventListener(`resize`, () => {
-//
-//     if(document.documentElement.clientWidth >= 1440)
-//     {listLength = 16} else {listLength = 5}
-//     console.log(listLength)
-//     getList(listLength ,arr)
-// }, false);
-
+// Формирование списка слайдов
 function getList(listLength, arr) {
-    console.log(i);
     let sliderList = document.getElementById("sliderList");
     let ul = document.createElement("ul");
     ul.id = "sliderList";
     for (; i < listLength; i++) {
         let li = document.createElement("li");
-        li.innerText = arr[i].li;
+        li.innerText = arr[i].name;
         ul.appendChild(li);
     }
-    if(ul.firstChild !== null )
-    ul.firstChild.id = "active";
-
+    i = 0;
+    if (ul.firstChild !== null)
+        ul.firstChild.id = "active";
+    let slide = arr.find(item => item.name === ul.firstChild.innerHTML);
+    document.getElementById("phrase").innerHTML = slide.text;
+    document.getElementById("title").innerText = slide.name;
     sliderList.replaceWith(ul);
     getSlide()
+
 }
 
-// function getSlide(e, arr) {
-//     let slide = arr.find(item => item.li === e.turget.innerHTML);
-// }
+document.addEventListener("DOMContentLoaded", () => sendRequest());
 
-document.addEventListener("DOMContentLoaded", ()=> getList(listLength, arr));
-
+// Отрисовка слайда
 function getSlide() {
-    let list = document.getElementById("sliderList");
+    const list = document.getElementById("sliderList");
     list.addEventListener("click", function (e) {
-        let slide = arr.find(item => item.li === e.target.innerHTML);
+        let slide = arr.find(item => item.name === e.target.innerHTML);
         document.getElementById("phrase").innerHTML = slide.text;
-        document.getElementById("title").innerText = slide.li;
-let listActive = document.getElementById('active');
-console.log(listActive)
-        if (listActive !== this)
-                    {
-                        listActive.removeAttribute("id");
-                    }
+        document.getElementById("title").innerText = slide.name;
+        let listActive = document.getElementById('active');
+        if (listActive !== this) {
+            listActive.removeAttribute("id");
+        }
         e.target.id = "active";
-
-        // let question = document.getElementsByClassName('question'),
-        //     blockActive = document.getElementsByClassName('blockActive');
-        // Array.from(list).forEach(function(item) {
-        //     item.addEventListener('click', function() {
-        //         if (blockActive.length > 0 && blockActive[0] !== this.parentNode)
-        //         {
-        //             blockActive[0].classList.remove('blockActive');
-        //         }
-        //         this.parentNode.classList.toggle('blockActive');
-        //     });
-        // });
     })
 }
