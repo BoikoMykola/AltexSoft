62f1e41a247aaa716a8c30a486d870c44ec74550
BoikoMykola <ukrelteh@gmail.com>
2021-06-11 00:17:41 +0300

    .gitignore is now working

---
+++ script.js
@@ -0,0 +1,76 @@
+const menuIcon = document.querySelector(".menu-icon");
+menuIcon.addEventListener("click", function () {
+    const navigate = document.getElementsByTagName("nav");
+    document.body.classList.toggle("lock")
+    navigate[0].classList.toggle("active");
+    menuIcon.classList.toggle("active")
+});
+
+
+// MENU HIDDEN
+let previous = 0;
+window.addEventListener("scroll", function () {
+    const menu = document.getElementsByClassName("header-container");
+    if (pageYOffset > 670 && pageYOffset < previous) {
+        document.getElementById("logo").src = "image/SKOUT2.png";
+        menu[0].classList.add("header-container-fixed");
+    } else {
+        document.getElementById("logo").src = "image/SKOUT.png";
+        menu[0].classList.remove("header-container-fixed");
+    }
+    previous = pageYOffset;
+});
+
+
+
+$(document).ready(function (){
+
+    //  GLOSSARY
+$('.slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 30000,
+    appendDots: $('.list')
+});
+
+//  PLANS
+    $('.plans').slick({
+        arrows: false,
+        slidesToShow: 1,
+        variableWidth: true,
+        autoplay: true,
+        autoplaySpeed: 30000,
+        responsive:[
+            {
+                breakpoint: 1440,
+                settings: {
+                    slidesToShow: 4,
+                    autoplay: false,
+                    centerMode: false,
+                    variableWidth: false,
+                    infinite: false
+                }
+            }
+        ],
+        mobileFirst: true
+    });
+
+    //  PARTNERS
+    $('.partners-big').slick({
+        arrows: false,
+        fade: true,
+        asNavFor: ".partners"
+    });
+    $('.partners').slick({
+        arrows: false,
+        slidesToShow: 5,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 30000,
+        appendDots: $('.partner-list'),
+        asNavFor: ".partners-big",
+        infinite: false
+    });
+});
+
